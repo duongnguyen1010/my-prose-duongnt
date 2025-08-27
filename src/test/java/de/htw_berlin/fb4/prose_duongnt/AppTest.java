8260f325d5d81b04162b105b15c319381d2a19f8
td <duongnt1010@gmail.com>
2025-08-27 21:36:17 +0200

    main: init maven project add junit5

---
+++ src/test/java/de/htw_berlin/fb4/prose_duongnt/AppTest.java
@@ -0,0 +1,16 @@
+package de.htw_berlin.fb4.prose_duongnt;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void appTest() {
+        assertEquals(5, 2 + 3);
+    }
+}
