76049b94b6d225397581b450f14eba0cd5e727b1
td <duongnt1010@gmail.com>
2025-08-27 22:48:28 +0200

    main: add copyright notices to codes

--- src/main/java/de/htw_berlin/fb4/prose_duongnt/App.java
+++ src/main/java/de/htw_berlin/fb4/prose_duongnt/App.java
@@ -1,9 +1,20 @@
+/**
+ * Notices:
+ *
+ * my-prose-duongnt
+ * Copyright (C) 2025 Thuy Duong Nguyen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This software includes third-party components under their own licenses.
+ *
+ * This project does not contain cryptography functionality.
+ */
+
 package de.htw_berlin.fb4.prose_duongnt;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
--- src/test/java/de/htw_berlin/fb4/prose_duongnt/AppTest.java
+++ src/test/java/de/htw_berlin/fb4/prose_duongnt/AppTest.java
@@ -1,5 +1,19 @@
-package de.htw_berlin.fb4.prose_duongnt;
+/**
+ * Notices:
+ *
+ * my-prose-duongnt
+ * Copyright (C) 2025 Thuy Duong Nguyen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This software includes third-party components under their own licenses.
+ *
+ * This project does not contain cryptography functionality.
+ */
 
+package de.htw_berlin.fb4.prose_duongnt;
 
 import org.junit.jupiter.api.Test;
 
